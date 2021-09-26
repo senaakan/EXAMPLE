3d17e777cf5aee725c789a92357967577c4028c2
senaakan <58777758+senaakan@users.noreply.github.com>
2021-09-26 16:07:46 +0300

    Update testbash.sh

--- testbash.sh
+++ testbash.sh
@@ -9,6 +9,11 @@ read dosyaismi
 if [ -e $dosyaismi ]
 then
 	echo "Dosya ismi bulundu"
+	while  read x1 x2 x3 x4
+
+        do
+                echo  ${x2}      ${x1}      ${x3}      ${x4}
+        done < $dosyaismi
 	sort -nk 4 $dosyaismi | tail -1
 else
 	echo "Dosya ismi bulunamadı"
